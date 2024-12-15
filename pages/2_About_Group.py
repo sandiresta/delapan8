38e08f1a69387ad892c89f1026014cf66b83a901
sandiresta <atseridnas23@gmail.com>
2024-12-15 13:00:06 +0700

    Update 2_About_Group.py

--- pages/2_About_Group.py
+++ pages/2_About_Group.py
@@ -4,9 +4,9 @@ st.title("About Our Group")
 st.write("Meet the amazing members of our group!")
 
 group_members = [
-    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\WhatsApp Image 2024-12-13 at 21.05.38_b1b3ada7.jpg"},
-    {"Name": "Sandi Resta", "Role": "DM ZP Leader", "Email": "sandi.resta@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\20220822_211922.jpg"},
-    {"Name": "Raden Asep Ahmad Fadillah", "Role": "2W BU Leader", "Email": "raden.fadillah@michelin.com", "Image": r"C:\Users\LENOVO\Documents\ImageProcessingApp - Group 8 - Add Logo and Photo\assets\WhatsApp Image 2024-12-13 at 21.06.02_107ccee3.jpg"},
+    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"assets/Ruddi_sutomi.jpg"},
+    {"Name": "Sandi Resta", "Role": "DM ZP Leader", "Email": "sandi.resta@michelin.com", "Image": r"assets/sandi_resta.jpg"},
+    {"Name": "Raden Asep Ahmad Fadillah", "Role": "2W BU Leader", "Email": "raden.fadillah@michelin.com", "Image": r"assets/raden_asep.jpg"},
 ]
 
 for member in group_members:
