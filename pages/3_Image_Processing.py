d50fc80ba3ed4aca6a3d0aefb70abfccaf40479e
sandiresta <atseridnas23@gmail.com>
2024-12-15 14:31:09 +0700

    Update 3_Image_Processing.py

--- pages/3_Image_Processing.py
+++ pages/3_Image_Processing.py
@@ -20,35 +20,4 @@ if uploaded_file:
     if st.checkbox("Convert to Grayscale"):
         grayscale_image = image.convert("L")
         st.image(grayscale_image, caption="Grayscale Image", use_column_width=True)
-# UPLOAD IMAGE CONTENT
-with tab3:
-    # VALIDATION IMAGE
-    def validate_image(image):
-        try:
-            img = Image.open(image)
-            img.verify() 
-        except Exception as e:
-            st.error("Invalid image file. Please upload a valid image file (.jpg, .jpeg, .png).")
-            return False
-
-        # Check file type
-        if not image.name.lower().endswith(('.jpg', '.jpeg', '.png')):
-            st.error("Invalid file type. Please upload a .jpg, .jpeg, or .png image.")
-            return False
-
-        # Check size
-        max_size_MB = 2  # 2 MB
-        max_size_bytes = max_size_MB * 1024 * 1024  # 2 MB in bytes
-        if image.size > max_size_bytes:
-            st.error(f"File size too large. Maximum allowed size is {max_size_MB} MB.")
-            return False
-
-        # Check dimensions
-        img_width, img_height = img.size
-        min_dim = 100  # Minimum dimension (width / height)
-        max_dim = 2000  # Maximum dimension (width / height)
-        if img_width < min_dim or img_width > max_dim or img_height < min_dim or img_height > max_dim:
-            st.error(f"Invalid image dimensions. Image dimensions should be between {min_dim}x{min_dim} and {max_dim}x{max_dim} pixels.")
-            return False
-        return True
 
