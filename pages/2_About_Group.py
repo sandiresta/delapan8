bbf2689a4ce4fe589453df63076911f2ea7b029f
sandiresta <atseridnas23@gmail.com>
2024-12-15 14:16:57 +0700

    Update 2_About_Group.py

--- pages/2_About_Group.py
+++ pages/2_About_Group.py
@@ -4,7 +4,7 @@ st.title("About Our Group")
 st.write("Meet the amazing members of our group!")
 
 group_members = [
-    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"assets/Ruddi_sutomi.jpg"},
+    {"Name": "Ruddi Sutomi", "Role": "FMU Leader", "Email": "ruddi.sutomi@michelin.com", "Image": r"assets/ruddi_sutomi.jpg"},
     {"Name": "Sandi Resta", "Role": "DM ZP Leader", "Email": "sandi.resta@michelin.com", "Image": r"assets/sandi_resta.jpg"},
     {"Name": "Raden Asep Ahmad Fadillah", "Role": "2W BU Leader", "Email": "raden.fadillah@michelin.com", "Image": r"assets/raden_asep.jpg"},
 ]
